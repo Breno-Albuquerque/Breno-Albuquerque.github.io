ee4ef95e51da8cd007a85313736337eb1ad76a1a
Breno Albuquerque <brenoralbu@hotmail.com>
2022-04-28 20:04:12 -0300

    feat: starting Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Bsymbol from '../subComponents/Bsymbol';
+
+const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: ${props => props.theme.black};
+
+  height: 10vh;
+`
 
 function Header() {
   return (
-    <div>OIIIIIIIIIIIIIIIIIIi</div>
+    <Container>
+      <Bsymbol/>
+    </Container>
   )
 }
 
---
+++ src/subComponents/Bsymbol.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Symbol = styled.div`
+  font-family: 'Indie Flower', cursive;
+  font-size: 50px;
+  color: ${props => props.theme.purple1};
+`
+
+function Bsymbol() {
+  return (
+    <Symbol>B</Symbol>
+  )
+}
+
+export default Bsymbol;
