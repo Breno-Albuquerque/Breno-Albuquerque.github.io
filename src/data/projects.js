9faebb2957c4168f26ef4529c41cb1a557ca87ec
Breno Albuquerque <brenoralbu@hotmail.com>
2022-04-29 13:36:47 -0300

    build: add data with projects

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    id: 1,
+    name: "App de receitas",
+    description: "A recipes application with multiple pages and functionalities",
+    link: "#",
+    techs: ['#React', '#Redux', '#Css'],
+    repository: '#'
+  },
+  {
+    id: 2,
+    name: "TrybeWallet",
+    description: "A virtual wallet able to store multiple expenses in different currency values.",
+    link: "https://trybewallet-b.vercel.app",
+    techs: ['#React', '#Redux', '#Css'],
+    repository: 'https://github.com/breno-albuquerque/trybewallet'
+  },
+  {
+    id: 3,
+    name: "Trivia Game",
+    description: "A trivia game with 5 random questions, timer, score, and ranking exibihtion",
+    link: "https://trivia-game-b.vercel.app",
+    techs: ['#React', '#Redux', '#Css'],
+    repository: 'https://github.com/breno-albuquerque/trivia-game'
+  },
+  {
+    id: 4,
+    name: "Memory Game",
+    description: "A simple memory game",
+    link: "#",
+    techs: ['#React', '#Redux', '#Css'],
+    repository: '#'
+  },
+  {
+    id: 5,
+    name: "Nutrients",
+    description: "A app to calculate and save your diet",
+    link: "#",
+    techs: ['#React', '#Redux', '#Css'],
+    repository: '#'
+  }
+]
