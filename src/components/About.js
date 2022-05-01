e86fd0d0d700355ba3b3f6ac9adcba4117c2509c
Breno Albuquerque <brenoralbu@hotmail.com>
2022-04-30 22:42:03 -0300

    feat: first version of svgs tech stack

--- src/components/About.js
+++ src/components/About.js
@@ -77,14 +77,14 @@ const SubTitle = styled.h4`
   font-size: 20px;
   color: ${props => props.theme.purple1};
 
-  margin-bottom: 16px;
+  margin: 32px 0;
 
   @media(min-width: 576px) {
     font-size: 22px;
   }
 `
 
-const ListContainer = styled.div`
+/* const ListContainer = styled.div`
   width: 100%;
   margin: 32px auto;
 
@@ -119,7 +119,7 @@ const ListItem = styled.li`
   @media(min-width: 576px) {
     font-size: 16px;
   }
-`
+` */
 
 const Highlight = styled.span`
   color: ${props => props.theme.purple1};
@@ -132,6 +132,44 @@ const SubContainer = styled.div`
 
   margin-top: 16px;
   width: 100%;
+`
+
+const SvgsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-evenly;
+
+  width: 90%;
+  max-width: 300px;
+
+  margin: 16px auto;
+`
+
+const SvgBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  border-radius: 0 50px 0 50px;
+
+  width: 130px;
+  height: 130px;
+
+  margin-bottom: 16px;
+
+  box-shadow: 4px 4px 4px  ${props => props.theme.purple2};
+
+  box-sizing: border-box;
+  padding: 8px;
+
+  border: 1px solid ${props => props.theme.purple2};
+`
+
+const SvgName = styled.span`
+  font-size: 14px;
+  color: ${props => props.theme.white};
 `
 
 function About() {
@@ -148,33 +186,54 @@ function About() {
         <Picture src={ myPicture2 } alt="Profile Picture"/>
       </SubContainer>
 
-      <Css />
-      <Docker />
-      <Git />
-      <Html />
-      <Jest />
-      <Js />
-      <Node />
-      <ReactSvg />
-      <Redux />
-      <Sql />
-      <Ts />
+      <SubTitle>#Tech Stack</SubTitle>
+    <SvgsContainer>
+      <SvgBox> 
+        <Css width={ 50 } />
+        <SvgName>Css</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Docker width={ 50 } />
+        <SvgName>Docker</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Git width={ 50 } />
+        <SvgName>Git</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Html width={ 50 } />
+        <SvgName>HTML</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Jest width={ 50 } />
+        <SvgName>Jest</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Js width={ 50 } />
+        <SvgName>JavaScript</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Node width={ 50 } />
+        <SvgName>Node.js</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <ReactSvg width={ 50 } />
+        <SvgName>React</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Redux width={ 50 } />
+        <SvgName>Redux</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Sql width={ 50 } />
+        <SvgName>MySQL</SvgName>
+      </SvgBox>
+      <SvgBox>
+        <Ts width={ 50 } />
+        <SvgName>TypeScript</SvgName>
+      </SvgBox>
+    </SvgsContainer>
 
-
-      <ListContainer>
-        <SubTitle>#Tech Stack</SubTitle>
-        <List>
-          <ListItem>HTML</ListItem>
-          <ListItem>CSS</ListItem>
-          <ListItem>JavaScript</ListItem>
-          <ListItem>React</ListItem>
-          <ListItem>Redux</ListItem>
-          <ListItem>Node.js</ListItem>
-          <ListItem>TypeScript</ListItem>
-          <ListItem>MySQL</ListItem>
-          <ListItem>Docker</ListItem>
-        </List>
-      </ListContainer>
     </Container>
   )
 }
