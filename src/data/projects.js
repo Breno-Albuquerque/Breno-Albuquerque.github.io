da02d3a0ef5600607459ff1053221426f306347f
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-08 22:07:05 -0300

    feat: Atualiza dados dos projetos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,38 @@
 export const projects = [
   {
     id: 1,
-    name: "Store Manager API",
-    description: "A Node.js with Express Back-end API to manager a MySQL store database.",
-    link: "https://github.com/breno-albuquerque/store-manager-back-end",
-    techs: ['#Node.js', '#Express', '#React', '#MySQL', '#Tests'],
-    repository: '#'
+    name: "Blog App",
+    description: "Node.js/Express API with React Front-end - A blog website where user can posts articles",
+    link: "https://my-blogs-app.netlify.app",
+    techs: ['#Node.js', '#Express', '#Sequelize', "#React"],
+    repository: 'https://github.com/breno-albuquerque/blogs-app'
   },
   {
     id: 2,
-    name: "TrybeWallet",
-    description: "A React/Redux virtual wallet able to store multiple expenses in different currency values.",
-    link: "https://trybewallet-b.vercel.app",
-    techs: ['#React', '#Redux', '#Css'],
-    repository: 'https://github.com/breno-albuquerque/trybewallet'
+    name: "Store Manager",
+    description: "Node.js/Express API with React Front-end - An app to manage product inventory and sales record",
+    link: "https://store-manager-app.netlify.app",
+    techs: ['#Node.js', '#Express', '#React', '#SQL', '#Tests'],
+    repository: 'https://github.com/breno-albuquerque/store-manager'
   },
   {
     id: 3,
+    name: "TrybeSmith",
+    description: "A Typescript Node Express API with MySQL database",
+    link: "",
+    techs: ["#Typescript", "#Node.js", "#Express", "MySQL"],
+    repository: 'https://github.com/breno-albuquerque/trybe-smith'
+  },
+  {
+    id: 4,
+    name: "Tech Store",
+    description: "Flask Python Virtual Store - create account, log in, search products, add and remove products from cart.",
+    link: null,
+    techs: ['#Flask', '#Python', '#sqlite', '#bootstrap'],
+    repository: 'https://github.com/breno-albuquerque/tech-store'
+  },
+  {
+    id: 5,
     name: "Trivia Game",
     description: "A React/Redux trivia game with 5 random questions, timer, score, and ranking exibihtion.",
     link: "https://trivia-game-b.vercel.app",
@@ -24,27 +40,11 @@ export const projects = [
     repository: 'https://github.com/breno-albuquerque/trivia-game'
   },
   {
-    id: 4,
-    name: "Tech Store",
-    description: "A virtual store where you can register, add and remove products from your cart",
-    link: "",
-    techs: ['#Flask', '#Python', '#sqlite', '#bootstrap', '#docker'],
-    repository: 'https://github.com/breno-albuquerque/trivia-game'
-  },
-  {
-    id: 5,
-    name: "Blog API Back-end",
-    description: "A React Front-end to display the Store Manager API",
-    link: "",
-    techs: ['#React', '#styled-components'],
-    repository: 'https://github.com/breno-albuquerque/blogs-api-back-end'
-  },
-  {
     id: 6,
-    name: "Blog API Front-end",
-    description: "A Node.js/Express API to manager a blog page with users and posts.",
-    link: "",
-    techs: ["Node.js", "Express", "Sequelize", "MySQL"],
-    repository: 'https://github.com/breno-albuquerque/blog-api-front-end'
+    name: "TrybeWallet",
+    description: "A React/Redux virtual wallet able to store multiple expenses in different currency values.",
+    link: "https://trybewallet-b.vercel.app",
+    techs: ['#React', '#Redux', '#Css'],
+    repository: 'https://github.com/breno-albuquerque/trybewallet'
   },
 ]
